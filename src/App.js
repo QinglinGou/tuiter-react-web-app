8ef4f569e4c49ec3b6dde69970246743b4b164b5
Chris <105400864+QinglinGou@users.noreply.github.com>
2023-04-02 21:08:24 -0400

    Update App.js
    test

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
 
+import { Navigate } from 'react-router-dom';
+
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="container">
-                <Routes>
-                    <Route path="/*" element={<Labs/>}/>
-                    <Route path={"/hello"} element={<HelloWorld/>}/>
-                    <Route path={"/tuiter/*"} element={<Tuiter/>}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Labs />} />
+          <Route path="/hello" element={<HelloWorld />} />
+          <Route path="/tuiter/*" element={<Tuiter />} />
+          {/* catch-all route */}
+          <Route path="*" element={<Navigate to="/tuiter" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
+
 export default App;
